3ac07b557244cdef1a032e0a9544da69cdf3dc5f
koshoji <ko.shoji@leon-tec.co.jp>
2022-03-23 15:22:21 +0900

    Add new project

---
+++ src/main/java/burp/BurpExtender.java
@@ -0,0 +1,7 @@
+package burp;
+
+public class BurpExtender implements IBurpExtender {
+    public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
+        // your extension code here
+    }
+}
